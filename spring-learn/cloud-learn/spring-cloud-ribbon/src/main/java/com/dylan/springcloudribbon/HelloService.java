62fccd6504073011788a15baaf6b6a8ff7c5d25f
dylan <395897448@qq.com>
2020-01-04 19:37:40 +0800

    添加Hystrix熔断

--- spring-learn/cloud-learn/spring-cloud-ribbon/src/main/java/com/dylan/springcloudribbon/HelloService.java
+++ spring-learn/cloud-learn/spring-cloud-ribbon/src/main/java/com/dylan/springcloudribbon/HelloService.java
@@ -1,5 +1,6 @@
 package com.dylan.springcloudribbon;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -21,7 +22,14 @@ public class HelloService {
 	 * @param name
 	 * @return
 	 */
+	@HystrixCommand(fallbackMethod = "hiError")//指定熔断的方法
 	public String hiService(String name) {
 		return restTemplate.getForObject("http://service-hi/hi?name="+name,String.class);
+	}
+
+
+
+	public String hiError(String name) {
+		return "hi,"+name+",sorry,error!";
 	}
 }
